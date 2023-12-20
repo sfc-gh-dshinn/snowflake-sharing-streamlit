09813dc03e2b895d7e52bb835d27d85a9f86f39e
Gilberto Hernandez <gilberto.hernandez@snowflake.com>
2023-12-19 21:08:34 -0600

    Add files

---
+++ main.py
@@ -0,0 +1,138 @@
+base = '''<html>
+  <head>
+		<title>How to use Snowflake to share data</title>
+		<style type="text/css">
+			@page {
+				size: letter;
+				margin: 5in;
+			}
+			body {
+				font-family: sans-serif;
+				margin: 4% 22%;
+			}
+			p {
+				line-height: 1.3em;
+			}
+			p.legal {
+				font-size: small;
+			}
+			img.header {
+				height: 62px;
+				width: auto;
+			}
+			img.snowflake {
+				height: 20px;
+				width: 20px;
+			}
+			img.step {
+				height: 400px;
+				width: auto;
+			}
+			h4 {
+				color: #29b5e8;
+			}
+			h4.end {
+				color: black;
+			}
+			a.end {
+				color: #29b5e8;
+			}
+		</style>
+	</head>
+	<body>
+		<img src="./assets/image3.jpg" class="header"/>
+		<h1>How to use Snowflake to share data</h1>
+		<p>
+		  ProviderName would like to share data with you via Snowflake to support your partnership and joint goals. This guide provides information about how to connect with ProviderName.
+		</p>
+		<p>
+			Snowflake can help answer your Snowflake-specific questions about data sharing and Snowflake account creation. If you're an existing Snowflake customer, contact your Snowflake account team for help; otherwise, please click here. 
+		</p>
+		<h3><span><img src="./assets/image1.png" class="snowflake"/></span> What is Snowflake?</h3>
+		<p>
+			Snowflake's Data Cloud is a global network where thousands of organizations mobilize data with near-unlimited scale, concurrency and performance. Snowflake provides organizations with a unified view of data so they can easily discover and securely share governed data, power data applications and execute diverse AI/ML and analytic workloads. All of this is delivered in a single data experience that spans multiple clouds and geographies.
+
+		</p>
+		<h3><img src="./assets/image1.png" class="snowflake"/> Why trust Snowflake?</h3>
+		<p>
+			Security is a core element of the Snowflake Platform and the Data Cloud. Layers of network security, identity and access management and end-to-end data encryption protect the data you load and use in Snowflake, helping to keep your data secure so you can spend more time analyzing it. For more details about our approach to security, including levels of compliance and certifications, please visit the <a href="https://www.snowflake.com/product/security-and-trust-center/">Snowflake Security and Trust Center</a>.
+		</p>
+		<h3><img src="./assets/image1.png" class="snowflake"/> 4 reasons to use Snowflake for data sharing</h3>
+		<ul>
+			<li><strong>It's easy. </strong>Connect in just a few clicks and start receiving data. With cross-cloud auto-fulfillment, data is available across clouds and regions when and where you need it. </li>
+			<li><strong>There's no need to move data.  </strong>Snowflake allows you to share data without transferring it. No need for ETL, FTP or moving data between environments. </li>
+			<li><strong>Robust data governance. </strong>You control access to your data in a centralized, flexible way. Share data with one other account, a group of accounts or to any company in the Data Cloud via the Snowflake Marketplace.</li>
+			<li><strong>Data is ready to use upon delivery. </strong>Snowflake delivers direct access to live, ready-to-query data. Perform fewer data transformations because the shared data stays in Snowflake, and easily join shared data sets with your own data.
+			</li>
+		</ul>
+		<h3><img src="./assets/image1.png" class="snowflake"/> How does Snowflake Data Sharing work?</h3>
+		<p>
+			ProviderName creates a share of a database in their Snowflake account and grants access to specific objects (tables, views and functions) in the database. You, as the consumer, will then see a read-only database in your Snowflake account. You can configure access to this database using standard role-based access control (RBAC).
+		</p>
+		<p>
+			For more specific details, see our <a href="https://docs.snowflake.com/en/user-guide/data-sharing-intro">Introduction to Snowflake Secure Data Sharing doc</a>.
+		</p>
+		<h3><img src="./assets/image1.png" class="snowflake"/> Terms and definitions</h3>
+		<p>
+			Collaboration depends on a shared, consistent understanding of common words and phrases so everyone can communicate clearly. Here are definitions of several terms that appear in this guide:
+		</p>
+		<ul>
+			<li><strong>Provider: </strong>The account that shares data</li>
+			<li><strong>Consumer: </strong>The account that receives data</li>
+			<li><strong>Share: </strong>A named Snowflake object that encapsulates all of the information required to share a database</li>
+			<li><strong>Direct share: </strong> A way to directly share specific database options (a share) to another account in the same region</li>
+			<li><strong>Listing: </strong>A way to offer a share plus metadata as a data product to one or more account</li>
+			<li><strong>Data Product: </strong>A reusable “building block” built to deliver data, data services, app code or data insights for a specific purpose</li>
+		</ul>
+		<h3><img src="./assets/image1.png" class="snowflake"/> How to receive data from ProviderName on Snowflake</h3>
+		<h4>If you are new to Snowflake</h3>
+		<p>
+			Getting started with Snowflake is easy, and it's a low-risk, no-commitment process.
+		</p>
+		<ul>
+			<li>You can begin with a free trial account that does not require a form of payment to set up</li>
+			<li>At the end of your trial, your trial account converts into an on-demand account where you only pay in arrears for the storage and compute resources you actually use; scale up and down as your business needs change</li>
+			<li>Your on-demand account will never expire and you will not be forced to make a long-term financial commitment</li>
+		</ul>
+		<p>To get a free Snowflake trial account, visit SPN_ReferralLink.</p>
+		<p><strong>NOTE</strong> During the sign-up process, you will choose the cloud region for your Snowflake instance. If you have no specific preference, we recommend you save time and costs by using the same cloud region as ProviderName: ProviderRegion.</p>
+		<h4>Once your account is set up OR if you are an existing Snowflake customer</h3>
+		<p>
+			For existing Snowflake customers, ProviderName will need your Snowflake account identifier to share into your account. To find your Snowflake account identifier, follow these steps:
+		</p>
+		<ol>
+			<li>Open the account selector and review the list of accounts.</li>
+			<img src="./assets/image2.png" class="step"/>
+			<br />
+			<br />
+			<br />
+			<li>Locate the account you want to use.</li>
+			<br />
+			<li>Hover over the account to view additional details and select the copy icon to copy the account identifier in the format &lt;orgname&gt;.&lt;account_name&gt; to your clipboard.</li>
+			<br />
+			<img src="./assets/image5.png" class="step"/>
+		</ol>
+		<br />
+		<p>
+			Once you have your account identifier, you'll need to provide it to ProviderName.
+		</p>'''
+
+shares='''<p>
+			You can view the ProviderName shares that are available to consume in your Snowflake account in the Snowsight UI. <strong>Select Data &gt;&gt; Private Sharing</strong>, then select <strong>Shared With You</strong>. You will see both privately shared listings and direct shares. 
+		</p>
+		<img src="./assets/image4.png" class="step"/>
+		<p>
+			For more details, including how to use the Snowflake Classic Console or SQL to  view available, please check out our <a href="https://docs.snowflake.com/en/user-guide/data-share-consumers#viewing-available-shares">"Consuming Shared Data" doc</a>. 
+		</p>'''
+
+reader='''<h4>If you do not have a Snowflake account and do not want to set one up, ProviderName can host a Snowflake Reader Account for you</h3>
+		<p>A Snowflake Reader Account is a quick, cost-effective way to share data with a non-Snowflake customer. The Reader Account belongs to the data provider and can only consume data from the provider that created it. Users in a Reader Account can query the data shared with the account, but cannot perform any write operations, such as ingesting new data into the account or storing results of queries run on the shared data (a full Snowflake account is required for those capabilities).</p>
+		<p>To request a Reader Account, please contact ProviderName.</p>'''
+
+        
+end='''<h4 class="end"><img src="./assets/image1.png" class="snowflake"/> Learn more about Snowflake Collaboration and Secure Data Sharing at <a href="https://www.snowflake.com/en/data-cloud/workloads/collaboration/" class="end">snowflake.com/en/data-cloud/workloads/collaboration</a></h3>
+		<p class="legal">
+			© 2023 Snowflake Inc. All rights reserved. Snowflake, the Snowflake logo, and all other Snowflake product, feature and service names mentioned herein are registered trademarks or trademarks of Snowflake Inc. in the United States and other countries. All other brand names or logos mentioned or used herein are for identification purposes only and may be the trademarks of their respective holder(s). Snowflake may not be associated with, or be sponsored or endorsed by, any such holder(s). 
+		</p>
+	</body>
+</html>'''
---
+++ ui.py
@@ -0,0 +1,126 @@
+from xhtml2pdf import pisa
+from main import base, shares, reader, end
+import streamlit as st
+from PIL import Image
+
+import os
+import re
+
+def write_html(pr_name, reg_name, link, st=None, rdr=False):
+    global base
+    global shares
+    global reader
+    html_file = open("snowflake_data_sharing.html", "w")
+
+    # Update all provider names
+    base = re.sub(r'ProviderName', pr_name, base)
+    shares = re.sub(r'ProviderName', pr_name, shares)
+    reader = re.sub(r'ProviderName', pr_name, reader)
+
+    # Update all region names
+    base = re.sub(r'ProviderRegion', reg_name, base)
+
+    # Update link
+    base = re.sub(r'SPN_ReferralLink', link, base)
+
+    # Has steps, supports reader
+    if st is not None and rdr is True:
+        html = base + shares + st + reader + end
+    elif st is not None:
+        # Has steps, does not support reader
+        html = base + shares + st + end
+    elif rdr is True:
+        html = base + shares + reader + end
+    else:
+        html = base + shares + end
+        
+
+    # convert HTML to PDF
+    # pisa_status = pisa.CreatePDF(
+    #     my_html,                # the HTML to convert
+    #     dest=html_file)           # file handle to recieve result
+
+    # close output file
+    html_file.write(html)
+    html_file.close() 
+
+def main():
+    
+    image = Image.open('./assets/image3.jpg')
+    st.image('./assets/image3.jpg')
+    
+    st.title("Create a Personalized Doc with Data Sharing Guidelines")
+
+    st.write("Simplify the process of sharing data with your customers by giving them a document that walks them through the process of connecting with you via Snowflake. This app will create a set of data sharing guidelines customized with your company's information to help your customer set up a Snowflake account and connect with you.")
+
+    st.write("To start, please input the requested information, including directions for how a customer can share their Snowflake ID with you. The app will generate a customized HTML document that you can save as a PDF and send to your customers.")
+
+    # Create a form
+    with st.form(key='user_input_form'):
+
+        # required
+        provider_name = st.text_input('Provider Name (required):')
+
+        # required
+        provider_region = st.text_input('Provider Region (required):')
+
+        # optional
+        spn_referral_link = st.text_input('SPN Referral Link:')
+
+        # optional
+        desc_steps = '''Customers will need to send you their Snowflake account identifier before you can share data with them. Please provide written instructions for how they should give you their account identifier. For example: "Send your account identifier to [email address]” or “Contact your account rep and give them your account ID."'''
+
+        steps = st.text_area(desc_steps)
+
+        # optional
+        reader = st.checkbox("Check this box if you will support reader accounts for companies that don't have a Snowflake account")
+        
+        submit_button = st.form_submit_button('Generate file')
+       
+    # Capture inputs
+    # When button is clicked:
+    # Check for errors
+    # Check for optional fields set
+    # Generate the file with required fields and optional fields
+    if submit_button:
+        # Validate inputs when button is clicked
+        if provider_name == '' or provider_region == '':
+            return st.error("File not generated. Required field is not set.")
+        
+        if spn_referral_link == '':
+            spn_referral_link = '<a href="https://signup.snowflake.com/">signup.snowflake.com</a>'
+        else:
+            spn_referral_link = f'<a href="{spn_referral_link}">{spn_referral_link}</a>'
+
+        # This won't render them in list order, instead 
+        # in block rendering, and it's hard to read
+        if steps != '':
+            steps = "<p>" + steps + "</p>"
+
+        if steps != '' and reader:
+            # render everything
+            write_html(provider_name, provider_region, spn_referral_link, steps, reader)
+        elif steps != '':
+            # render portion
+            write_html(provider_name, provider_region, spn_referral_link, steps)
+        elif reader:
+            write_html(provider_name, provider_region, spn_referral_link, reader)
+        else:
+            write_html(provider_name, provider_region, spn_referral_link)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# # Render a st.button that creates the pdf on click
+# st.button("Generate PDF", 
+#     type="primary",
+#     on_click=convert_html_to_pdf(provider_name, provider_region, spn_referral_link, 'myfile.pdf')
+# );
+
+
+# define a create pdf functgion that takes the dict, generates the PDF with values in dict
+# if __name__ == "__main__":
+    # create_pdf(title)
