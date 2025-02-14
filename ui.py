d1b4215a49c5ff5d8fa6b2bad8c059c7dc69bf72
David Shinn <david.shinn@snowflake.com>
2025-02-14 15:24:58 -0500

    Add final draft form changes for Provider and Consumer.

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 import re
 
-def write_html(pr_name, reg_name, link, st=None, rdr=False):
+def write_html(pr_name, reg_name, link, st=None, rdr=False, m_accts=False):
     import jinja2
 
     from main import template_text
@@ -18,7 +18,8 @@ def write_html(pr_name, reg_name, link, st=None, rdr=False):
         region_name=reg_name,
         spn_referral_link=link,
         steps=st,
-        reader=rdr
+        reader=rdr,
+        managed_accounts=m_accts
     )
 
     # close output file
@@ -75,30 +76,43 @@ def main():
 
     st.title("Customize a Guidelines Doc for Sharing Data via Snowflake")
 
-    st.write("Simplify the process of sharing data with your customers: give them a how-to-share document customized with your company's name and preferences. This app will create a set of data sharing guidelines to help your customer connect with you through their existing Snowflake account or by setting up a new Snowflake account.")
+    st.write("Simplify the process of sharing data with your customers or requesting data from providers: give them a how-to-share document customized with your company's name and preferences. This app will create a set of data sharing guidelines to help your customer or your provider connect with you through their existing Snowflake account or by setting up a new Snowflake account.")
 
     st.markdown("[Click here to see a sample PDF.](https://www.snowflake.com/wp-content/uploads/2024/03/Sample-Doc-How-to-Use-Snowflake-to-Share-Data.pdf)")
 
-    st.write("To get started, please fill out this form as completely as possible. The app will generate a customized HTML document that you can save as a PDF and send to your customers. You can return to this form and create additional versions as needed.")
+    st.write("To get started, please select if you want to provide or receive data and fill out the form as completely as possible. The app will generate a customized HTML document that you can save as a PDF and distribute. You can return to the app and create additional versions as needed.")
 
-    st.markdown('**Choose the type of form you need**')
-    form_type = st.selectbox(label="", index=None, options=["Provider", "Consumer"], label_visibility="collapsed")
+    st.markdown('**Choose the type of document you need**')
+    form_type = st.selectbox(label="", index=None, options=["Receiver", "Provider"], format_func=lambda x: {"Receiver": "I want to receive data", "Provider": "I want to provide data"}[x], label_visibility="collapsed")
 
     if form_type:
         # Create a form
         with st.form(key='user_input_form'):
+            # Name required
+            if form_type == 'Provider':
+                name_field_name = "Provider Name"
+            else:
+                name_field_name = "Your Company Name"
+            st.markdown(f'**{name_field_name} (required)**')
+            name = st.text_input('How do you want to refer to your organization in this document? The text you enter will be used to customize the document.')
 
-            # required
-            st.markdown(f'**{form_type} Name (required)**')
-            provider_name = st.text_input('How do you want to refer to your organization in this document? The text you enter will be used to customize the document.')
-
-            # required
-            st.markdown(f'**{form_type} Region (required)**')
-            provider_region = st.selectbox(
-                label='A customer will use this value to select a cloud region for their own Snowflake instance. Please check the list of supported Snowflake regions at https://docs.snowflake.com/en/user-guide/intro-regions and populate this field with your relevant region name (for example: "AWS US East (Ohio)"). This will make it easy for the customer to match it to the region options they see during account setup.',
-                options=region_list,
-                index=None
-                )
+            # Region required
+            if form_type == 'Provider':
+                region_field_name = "Provider Region"
+                st.markdown(f'**{region_field_name} (required)**')
+                region = st.selectbox(
+                    label='Please select your relevant cloud region from the menu. This will make it easy for the customer to match it to the region options they see during account setup.',
+                    options=region_list,
+                    index=None
+                    )
+            else:
+                region_field_name = "Your Cloud Region"
+                st.markdown(f'**{region_field_name} (required)**')
+                region = st.selectbox(
+                    label='Please select your relevant cloud region from the menu. This will make it easy for the provider to match it to the region options they see during account setup.',
+                    options=region_list,
+                    index=None
+                    )
 
             # optional
             if form_type == 'Provider':
@@ -107,14 +121,25 @@ def main():
             else:
                 spn_referral_link = ''
 
-            # optional
-            st.markdown('**Instructions to submit Snowflake ID**')
-            desc_steps = '''Customers will need to send you their Snowflake account identifier before you can share data with them. Please provide written instructions for how they should give you their account identifier. For example: "Send your account identifier to [email address]” or “Contact your account rep and give them your account ID."'''
+            # Instructions optional
+            if form_type == 'Provider':
+                st.markdown('**Instructions to submit Snowflake ID**')
+                desc_steps = '''Customers will need to send you their Snowflake account identifier before you can share data with them. Please provide written instructions for how they should give you their account identifier. For example: "Send your account identifier to [email address]” or “Contact your account rep and give them your account ID."'''
+                steps = st.text_area(desc_steps)
+            else:
+                steps = ''
 
-            steps = st.text_area(desc_steps)
+            # reader accounts optional
+            if form_type == 'Provider':
+                reader = st.checkbox("Check this box if you will support reader accounts for companies that don't have a Snowflake account.")
+            else:
+                reader = False
 
-            # optional
-            reader = st.checkbox("Check this box if you will support reader accounts for companies that don't have a Snowflake account.")
+            # managed accounts optional
+            if form_type == 'Provider':
+                managed_accounts = False
+            else:
+                managed_accounts = st.checkbox("**Check this box if you will support Managed Accounts for companies that don’t have a Snowflake account.**  Selecting this option will add language that describes a Managed Account and invites the provider to contact your organization to request a Managed Account.")
 
             submit_button = st.form_submit_button('Submit')
 
@@ -125,8 +150,10 @@ def main():
         # Generate the file with required fields and optional fields
         if submit_button:
             # Validate inputs when button is clicked
-            if provider_name == '' or provider_region == '':
-                return st.error("File not generated. Required field is not set.")
+            if name == '':
+                return st.error(f"File not generated. Required field **{name_field_name}** is not set.")
+            elif not region:
+                return st.error(f"File not generated. Required field **{region_field_name}** is not set.")
 
             if spn_referral_link == '':
                 spn_referral_link = '<a href="https://signup.snowflake.com/">signup.snowflake.com</a>'
@@ -135,7 +162,7 @@ def main():
 
             # This won't render them in list order, instead 
             # in block rendering, and it's hard to read
-            write_html(provider_name, provider_region, spn_referral_link, steps, reader)
+            write_html(name, region, spn_referral_link, steps, reader)
 
             f = open("snowflake_data_sharing.html", "r")
 
